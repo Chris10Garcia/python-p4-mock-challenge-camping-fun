40f60ea3e7e89ada5aed1d845baf7db5f2092490
Christian Garcia <bones192nd@gmail.com>
2023-10-18 17:00:45 -0400

    almost done

--- server/app.py
+++ server/app.py
@@ -5,7 +5,9 @@ from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
 import os
+from sqlalchemy import exc
 
+ 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
@@ -24,6 +26,93 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+@app.route("/campers", methods=["GET", "POST"])
+def campers():
+
+    if request.method == "GET":
+        campers = Camper.query.all()
+
+        campers_serialized = [camper.to_dict(only=("id","name", "age")) for camper in campers]
+        response = make_response(
+            jsonify(campers_serialized),
+            200
+        )
+        return response
+    elif request.method == "POST":
+        data = request.get_json()
+        camper = Camper(**data)
+
+        db.session.add(camper)
+        
+        try:
+            db.session.commit()
+            code = 201
+            response = make_response(
+                jsonify(camper.to_dict()),
+                code
+            )
+            return response
+        
+        except exc.SQLAlchemyError as e:
+
+            response = make_response(
+                jsonify({"errors": ["validation errors"]}),
+                400
+            )
+            return response
+
+        
+    
+
+@app.route("/campers/<int:id>", methods=["GET", "PATCH"])
+def campers_by_id(id):
+    if request.method == "GET":
+        camper = Camper.query.filter(Camper.id == id).first()
+
+        if not camper:
+            camper_serialized = {"error": "Camper not found"}
+            code = 404
+        else:
+            camper_serialized = camper.to_dict()
+            code = 200
+
+        response = make_response(
+            jsonify(camper_serialized),
+            code
+        )
+
+        return response
+    
+    elif request.method == "PATCH":
+        camper = Camper.query.filter(Camper.id == id).first()
+
+        if not camper:
+            camper_serialized = {"error": "Camper not found"}
+            code = 404
+
+        else:
+            data = request.get_json()
+            [setattr(camper, attr, data[attr]) for attr in data]
+
+            try:
+                db.session.add(camper)
+                db.session.commit()
+                camper_serialized = camper.to_dict()
+                code = 202
+
+            except:
+                camper_serialized = {"errors": ["validation errors"]}
+                code = 400
+
+            finally:
+                response = make_response(
+                    jsonify(camper_serialized),
+                    code
+                )
+
+                return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,7 +11,7 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
-
+ 
 metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
@@ -25,8 +25,12 @@ class Activity(db.Model, SerializerMixin):
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
+    # campers = db.relationship("Camper", secondary="signups", back_populates="activities")
+    signups = db.relationship("Signup", back_populates="activity", cascade="delete")
+
     # Add serialization rules
+    serialize_rules = ('-signups.activity',)
+
     
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -40,11 +44,27 @@ class Camper(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Add relationship
-    
+    # activities = db.relationship("Activity", secondary="signups", back_populates="campers", overlaps="signups" )
+    signups = db.relationship("Signup", back_populates="camper", cascade="delete")
+
     # Add serialization rules
-    
+    serialize_rules = ('-signups.camper',)
+
     # Add validation
     
+    @validates("name")
+    def check_name(self, key, address):
+        if address == "":
+            return ValueError("Please include name")
+        
+        return address
+
+    @validates("age")
+    def age_limits(self, key, address):
+        if address < 8 or address > 18:
+            return ValueError("Please be between 8 and 18 years of age")
+
+        return address   
     
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -55,12 +75,24 @@ class Signup(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
 
     # Add relationships
-    
+    activity = db.relationship("Activity", back_populates="signups", )
+    camper = db.relationship("Camper", back_populates="signups")
+
     # Add serialization rules
-    
+    serialize_rules = ('-camper.signups', '-activity.signups',)
+
     # Add validation
+
+    @validates("time")
+    def age_limits(self, key, address):
+        if address < 0 or address > 23:
+            return ValueError("Please have a time between 0 and 23 hours")
+
+        return address   
     
     def __repr__(self):
         return f'<Signup {self.id}>'
